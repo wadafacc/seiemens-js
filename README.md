f0aa275cf2adc10abe4f7e0099616f2d79c6ab65
Wadafacc <70700633+Wadafacc@users.noreply.github.com>
2021-07-15 12:03:57 +0200

    yee

---
+++ README.md
@@ -0,0 +1,2 @@
+<b>THE HOLY GRAIL</b>
+This package allows you to use a variety of functions which were made according to the *Seiemens* Standard.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 function seiemens(ttl) {
     setInterval(() => {
-        console.log("Seiemens Remaining: YES");
-    }, ttl);
+        console.log("Time Remaining: YES");
+    }, ttl * 10);
 }
+
+function checkForSeiemens(text) {
+    if (text.includes("Seiemens")) {
+        console.log("No Errors were found.");
+        return text;
+    }
+    else {
+        text += " Seiemens. ";
+        console.log("Fixed all Errors.");
+        return text;
+    }
+}
+
 
 module.exports = {
     seiemens,
+    checkForSeiemens,
 }
